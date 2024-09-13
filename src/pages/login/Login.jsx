b3ef38de2827af1d6309c1b05b27f5bdc19317ae
emess2g <essuonemma84gmail.com>
2024-09-13 13:22:40 +0000

    added sign up / login logic

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 const Login = () => {
+    const  [currState, setCurrState] = useState("Sign up");
+
   return (
     <div className='login'>
       <img src={assets.logo_big} className='logo' alt="" />
       <form action="" className='login-form'>
-        <h2>Sign Up</h2>
-        <input type="text" placeholder='username' className="form-input" required />
-        <input type="email" placeholder='email address' className="form-input" />
-        <input type="password" placeholder='password' className="form-input" />
-        <button type='submit'>Sign Up</button>
+        <h2>{currState}</h2>
+        {currState === "Sign up" ? <input type="text" placeholder='username' className="form-input" required /> : null}
+        <input type="email" placeholder='email address' className="form-input" required />
+        <input type="password" placeholder='password' className="form-input" required />
+        <button type='submit'>{currState === "Sign up" ? "Create account":"Login now"}</button>
         <div className='login-term'>
              <input type="checkbox" />
              <p>Agree to the term of use & privacy policy</p>
         </div>
         <div className="login-forgot">
-            <p className="login-toggle">
+            {
+                currState === "Sign up" ? 
+                 <p className="login-toggle">
                 Already have an account 
-                <span> Click Here</span>
-            </p>
+                <span onClick={() => setCurrState('Login')}> Login Here</span>
+                </p>
+                :
+                <p className="login-toggle">
+                Create an account 
+                <span onClick={() => setCurrState('Sign up')}> Click Here</span>
+                </p>
+            }
+           
+
         </div>
       </form>
     </div>
