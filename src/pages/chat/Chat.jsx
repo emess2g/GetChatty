58ec90e95d2710115295fe0fa3c1fd88a5c4ba4e
emess2g <essuonemma84gmail.com>
2024-09-13 17:38:03 +0000

    added chatbox files

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/leftSidebar/LeftSidebar'
+import ChatBox from '../../components/chatBox/ChatBox'
+import RightSidebar from '../../components/rightSidebar/RightSidebar'
+
 
 const Chat = () => {
   return (
-    <div>
-      chat
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+      </div>
     </div>
   )
 }
