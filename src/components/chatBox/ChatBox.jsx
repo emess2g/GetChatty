5c53dccdbd2f5e2c794b801b52f1286fd2662850
emess2g <essuonemma84gmail.com>
2024-09-16 11:49:22 +0000

    added chat input mark up

--- src/components/chatBox/ChatBox.jsx
+++ src/components/chatBox/ChatBox.jsx
@@ -6,8 +6,16 @@ const ChatBox = () => {
     <div className='chat-box'>
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
-        <p>Richard Stanford <img src={assets.green_dot} alt="" /></p>
-        <img src={assets.help_icon} className='help' alt="" />
+        <p>Richard Stanford <img className='img-dot' src={assets.green_dot} alt="" /></p>
+        <img  src={assets.help_icon} className='help' alt="" />
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder='sender a message' />
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   )
