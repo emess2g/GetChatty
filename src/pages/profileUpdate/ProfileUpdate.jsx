f589da9879e1a1f1ef977042f308e606c90209af
emess2g <essuonemma84gmail.com>
2024-09-17 14:38:01 +0000

    added profile update mark up

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate = () => {
   return (
-    <div>
-      profile
+    <div className='profile'>
+       <div className="profile-container">
+        <form action="">
+          <h3>Profile Update</h3>
+          <label htmlFor="avatar">
+             <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+             <img src={assets.avatar_icon} alt="" />
+              upload profile image
+          </label>
+          <input type="text" placeholder='Your name' required />
+          <textarea placeholder='Write profile bio'></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+       </div>
     </div>
   )
 }
