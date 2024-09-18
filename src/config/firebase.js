cf8936c79f0370c1cff6bee883c7f469a028bd11
emess2g <essuonemma84gmail.com>
2024-09-18 08:59:22 +0000

    imported firebase config file

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0aSmSTOzMdSGrXrb7VnyWDS5AjISuRH4",
+  authDomain: "getchatty-4187d.firebaseapp.com",
+  projectId: "getchatty-4187d",
+  storageBucket: "getchatty-4187d.appspot.com",
+  messagingSenderId: "80897855384",
+  appId: "1:80897855384:web:9745358ba327ec3ca8e5ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
