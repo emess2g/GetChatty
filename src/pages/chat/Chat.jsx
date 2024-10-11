328b69122b991679d2ee181031dfab5005ccbeb0
emess2g <essuonemma84gmail.com>
2024-10-11 10:42:46 +0000

    added loading when data is being fetched

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Chat.css'
 import LeftSidebar from '../../components/leftSidebar/LeftSidebar'
 import ChatBox from '../../components/chatBox/ChatBox'
 import RightSidebar from '../../components/rightSidebar/RightSidebar'
+import { AppContext } from '../../context/AppContext'
 
 
 const Chat = () => {
+  const {chatData,userData} = useContext(AppContext);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+      if(chatData && userData){
+        setLoading(false)
+      }
+  }, [chatData, userData])
+
   return (
     <div className='chat'>
-      <div className="chat-container">
+      {
+        loading ? <p className='loading'>Loading...</p> 
+        : <div className="chat-container">
         <LeftSidebar/>
         <ChatBox/>
         <RightSidebar/>
       </div>
+      }
+    
     </div>
   )
 }
